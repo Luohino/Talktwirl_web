47924393e36a2c0a36c6463c7b1e8c89b07a2c0c
Luohino <aniketsingh821305@gmail.com>
2025-07-02 03:00:54 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Talktwirl_web/', // Add this line
   plugins: [react()],
 })
